59c307ff92f03ed01bffaf2e36a9d80d014f5ca9
viniciusfragak <75163496+viniciusfragak@users.noreply.github.com>
2021-10-13 18:13:09 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 13 14:56:24 2021
+
+@author: ViniciusFraga
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Index!"
+
+@app.route("/hello")
+def hello():
+    return "Hello World!"
+
+@app.route("/members")
+def members():
+    return "Members"
+
+@app.route("/members/<string:name>/")
+def getMember(name):
+    return "name/<string:name>"
+
+if __name__ == "__main__":
+    app.run()
